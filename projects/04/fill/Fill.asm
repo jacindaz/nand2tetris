fbb27ec8235eafa5607c365adfc1be87188c6096
Jacinda <jacindaz@gmail.com>
2017-12-21 17:30:42 -0500

    Keyboard problem: add ability to read the code of pressed key
     - read in the code from RAM[24576] (register that the pressed
       key is stored in)
     - grab the value in RAM[24576]
     - save the value of that scan code into register 17

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,28 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+//
+// Tips for keyboard listener homework problem:
+// * use a loop with pointers to continuously listen for any keyboard input
+// * need to fill every pixel (all memory addresses) with black or white
 
-// Put your code here.
+(LOOP)
+  // this bit is to store @SCREEN's address into register 16
+  @KBD
+  D=A
+  @addr
+  M=D
+
+  // try to store the value of RAM[24576] into
+  // @keyboardValue (register 17)
+  @KBD
+  D=M
+  @keyboardValue
+  M=D
+
+  @LOOP
+  0;JMP
